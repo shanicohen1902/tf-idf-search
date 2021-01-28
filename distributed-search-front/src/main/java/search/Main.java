ed4685e22639ab99b27688c72fa28ae71fe74950
Shani Cohen <shani.cohen@tikalk.com>
2021-01-28 15:51:37 +0200

    Kubernetes support

--- distributed-search-front/src/main/java/search/Main.java
+++ distributed-search-front/src/main/java/search/Main.java
@@ -15,7 +15,7 @@ public class Main  {
     public static void main(String[]args) throws IOException {
 		String zookeeperConnectionString = "";
 		String serverPort = "";
-		System.out.println("argsu: " + args.length);
+		System.out.println("args: " + args.length);
 
 		if(System.getenv("ZOO_ADDRESS") != null){
 			System.out.println("sys prp " + System.getenv("ZOO_ADDRESS"));
